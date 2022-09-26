b3401c9eeb2db04e19c091840e17d6a18993c261
Glenn <pounguiglennstephen@gmail.com>
2022-09-26 22:25:28 +0100

    Pas de changement majeur !

--- main.py
+++ main.py
@@ -5,26 +5,40 @@ class Workers:
         print()
         self.first_name = self.get_first_name()
         self.last_name = self.get_last_name()
-        self.old = self.get_old()
+        self.sex = self.get_sex()
+        self.birthday = self.get_birthday()
         self.address = self.get_address()
 
     def get_first_name(self):
         self.first_name = input("Quel est votre nom ? ")
-        return self.first_name
+        if self.first_name == "":
+            print("Veillez entrer un nom !")
+            self.get_first_name()
+        return self.first_name.upper()
 
     def get_last_name(self):
         self.last_name = input("Quel est votre prénom ? ")
-        return self.last_name
+        if self.last_name == "":
+            print("Veillez entrer un prénom !")
+            self.get_last_name()
+        return self.last_name.title()
+
+    def get_sex(self):
+        self.sex = input("Quel est votre sex (H ou F) ? ")
+        if self.sex.upper() != "H" and self.sex.upper() != "F":
+            print("Veillez répondre par H ou F !")
+            self.get_sex()
+        return self.sex.upper()
     
-    def get_old(self):
-        self.old = input("Quel est votre age ? ")
-        if not self.old.isdigit() or int(self.old) >= 100:
-            print("Veillez entrer votre age réel !")
-            self.get_old()
-        return self.old
+    def get_birthday(self):
+        self.birthday = input("Quel est votre date de naissance (DD/MM/YYYY) ? ")
+        return self.birthday
             
     def get_address(self):
         self.address = input("Quelle est votre adresse ? ")
+        if self.address == "":
+            print("Veillez entrer votre adresse actuelle !")
+            self.get_address()
         return self.address
 
 class Administration(Workers):
@@ -34,10 +48,15 @@ class Administration(Workers):
 class Teacher(Workers):
     def __init__(self):
         super().__init__()
+        matricule
+        profession
 
 class Student(Workers):
     def __init__(self):
         super().__init__()
+        matricule
+        filière
+
 
 class Maintenance(Workers):
     def __init__(self):
@@ -47,3 +66,4 @@ class Maintenance(Workers):
 if __name__ == "__main__":
     worker = Workers()
     worker
+    print(worker.first_name, worker.last_name)
